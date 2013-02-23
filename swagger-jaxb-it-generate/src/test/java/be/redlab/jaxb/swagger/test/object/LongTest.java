fca7a9fa326b76a290d1e08ba084afaf70304925
redlab <balder@redlab.be>
2013-02-23 16:14:53 +0100

    more tests, to check the Wrapper versions of primitives when minOccurs="0"

---
+++ swagger-jaxb-it-generate/src/test/java/be/redlab/jaxb/swagger/test/object/LongTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2013 Balder Van Camp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package be.redlab.jaxb.swagger.test.object;
+
+import java.lang.reflect.Method;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.example.api.model.Root;
+import com.wordnik.swagger.annotations.ApiProperty;
+
+/**
+ * XSD type long mapped to Swagger long
+ *
+ * @author redlab
+ *
+ */
+public class LongTest {
+
+	private Method method;
+	private ApiProperty apiProperty;
+
+	@Before
+	public void setup() throws NoSuchMethodException, SecurityException {
+		method = Root.class.getMethod("getLongElement");
+		apiProperty = method.getAnnotation(ApiProperty.class);
+	}
+
+	@Test
+	public void annotationNotNull() {
+		failIfNoAnnotation();
+	}
+
+	@Test
+	public void typeIsCorrect() {
+		failIfNoAnnotation();
+		Assert.assertEquals("long", apiProperty.dataType());
+	}
+
+	@Test
+	public void nameIsCorrect() {
+		failIfNoAnnotation();
+		Assert.assertEquals("longElement", apiProperty.value());
+	}
+
+	@Test
+	public void required() {
+		failIfNoAnnotation();
+		Assert.assertEquals(true, apiProperty.required());
+	}
+
+	protected void failIfNoAnnotation() {
+		Assert.assertNotNull("ApiProperty not found on method", apiProperty);
+	}
+}
